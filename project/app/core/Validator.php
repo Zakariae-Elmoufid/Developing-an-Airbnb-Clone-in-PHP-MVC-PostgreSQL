5a9a6708ee60eaf159e212cf370c016b6d9a3e04
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-02-07 17:01:41 +0100

    create class validator for vallide input

---
+++ project/app/core/Validator.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace App\core;
+
+class Validator{
+
+    static $result;
+
+
+    static function name($name){
+        if(!preg_match(preg_match("/^[a-zA-Z0-9_\s]{3,20}$/",  $name))){
+            self::$result=false;
+        }
+        else{
+            self::$result=true;
+        }
+        return self::$result;
+    }
+
+
+
+   
+}
