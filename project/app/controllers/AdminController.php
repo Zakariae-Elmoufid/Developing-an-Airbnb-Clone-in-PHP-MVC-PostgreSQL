86201b365aed523fce5a097523ee3eae0a94beb3
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-02-11 23:11:36 +0100

    J'ai termin la connexin deu database

--- project/app/controllers/AdminController.php
+++ project/app/controllers/AdminController.php
@@ -2,12 +2,14 @@
 namespace App\controllers;
 
 use App\core\Controller;
+use App\config\Database;
 
 
 class AdminController extends Controller{
 
   public function index(){
     $this->view('admin/index'); 
+
   }
 
 
