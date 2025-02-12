aceb753ff4872e553d93d9a9fec0e09cc20d3873
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-02-12 10:15:18 +0100

    J'ai termin la function de validation de form et de select data de form

--- project/app/router/web.php
+++ project/app/router/web.php
@@ -6,6 +6,7 @@ use App\core\Router;
 Router::add("GET","/","HomeController@index");
 Router::add("GET","/admin","AdminController@index");
 Router::add("GET","/categories","AdminController@categories");
+Router::add("POST","/categories","AdminController@addCategories");
 Router::add("GET","/accommodation","AdminController@accommodation");
 Router::add("GET","/conflits","AdminController@conflits");
 Router::add("GET","/avis","AdminController@avis");
--- project/public/asset/js/main.js
+++ project/public/asset/js/main.js
@@ -8,7 +8,7 @@ function ouverModal(modal){
     modal.classList.remove('hidden');
 }
 
-formAdd.addEventListener("submit",(e)=>{
+formAdd.addEventListener("submit",async (e)=>{
     e.preventDefault();
     const formData = new FormData(formAdd);
     console.log(formData);
@@ -17,6 +17,50 @@ formAdd.addEventListener("submit",(e)=>{
     formData.forEach((value, key) => {
         data[key] = value;
     });
-    console.log(data);
+    console.log(JSON.stringify(data));
+    
+    if (!formAdd.checkValidity()) {
+        e.stopPropagation();
+        formAdd.querySelectorAll(":invalid").forEach(input => {
+            input.classList.add("border-red-500");
+            input.nextElementSibling.classList.remove("hidden");
+            return false;
+        });
+    }else{
+        // Change button text to indicate the process is ongoing
+document.getElementById("addCategoriesBtn").value = 'Please Wait ...';
+
+try {
+    // Envoyer les données sous forme de JSON
+    const response = await fetch('categories', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data), // Convertir l'objet JavaScript en JSON
+    });
+
+    // Affichage dans la console pour vérifier les données envoyées
+    console.log(JSON.stringify(data));
+
+    // Vérification du statut de la réponse
+    if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+    }
+
+    // Récupérer la réponse du serveur
+    const result = await response.text();
+
+    // Afficher la réponse dans l'élément 'alert'
+    alert.innerHTML = result;
+    } catch (error) {
+    // En cas d'erreur, rétablir le texte du bouton et afficher l'erreur
+    document.getElementById("addCategoriesBtn").value = 'Try Again';
+    alert.innerHTML = `An error occurred: ${error.message}`;
+    }
+
+
+
+    }
     
 })
