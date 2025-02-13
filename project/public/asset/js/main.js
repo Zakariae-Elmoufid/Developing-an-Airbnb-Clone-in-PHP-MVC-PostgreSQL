51939e81e566427bd4254db77db5163036e3b8da
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-02-13 01:05:06 +0100

    J'ai termin la function de select id  categories

--- project/public/asset/js/main.js
+++ project/public/asset/js/main.js
@@ -87,12 +87,12 @@ function displayCategories(array){
         <td class="px-6 py-4 text-gray-600">${element['title']}</td>
         <td class="px-6 py-4">
             <div class="flex items-center justify-end gap-2">
-                <button  onclick="ouverModal(modalUpdateCategories)" class="text-gray-400 hover:text-blue-500 transition-colors">
-                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
+                <a id="${element['id']}" onclick="ouverModal(modalUpdateCategories)" class="text-gray-400 hover:text-blue-500 transition-colors editLink">
+                    <svg  class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path  stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
                     </svg>
-                </button>
-                <button class="text-gray-400 hover:text-red-500 transition-colors">
+                </a>
+                <button id="${element['id']}" class="text-gray-400 hover:text-red-500 transition-colors ">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
                     </svg>
@@ -104,3 +104,21 @@ function displayCategories(array){
     });
    
 }
+
+tbody.addEventListener("click", (e) => {
+    let target = e.target.closest('.editLink');
+    if (target) {
+        let id = target.getAttribute("id");
+        console.log(id);
+        // editUser(id);
+    }
+});
+ 
+const remplireFormUpdate = async (e)=>{
+    const data = await fetch("getCategorieById",{
+        method: "GET",   
+    })
+    const response= await data.json();
+    console.log(response);
+}
+
