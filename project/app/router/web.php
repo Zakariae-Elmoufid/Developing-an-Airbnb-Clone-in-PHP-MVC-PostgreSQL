37eca21da6a5d78cee6299174266c007a2b51a9a
khaoula-benrhebra <khaoulabenrhebra@gmail.com>
2025-02-14 09:11:37 +0100

    add to database

--- project/app/router/web.php
+++ project/app/router/web.php
@@ -3,5 +3,42 @@
 use App\core\Router; 
 Router::add("GET","/","HomeController@index");
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Router::add("GET","/dashboard","OwnerController@dashboardCategories");
 Router::add("POST","/dashboard/create", "OwnerController@create");
